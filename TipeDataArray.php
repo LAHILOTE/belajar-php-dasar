3bb5df0d2174dc4ea2172eb10c4bea4952780659
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-25 03:45:04 +0800

    Array Map

--- TipeDataArray.php
+++ TipeDataArray.php
@@ -4,7 +4,37 @@ var_dump($values);
 
 
 $names = ["Machmud", "Wahyu", "Domili"];
-var_dump($names)
+var_dump($names);
+
+var_dump($names[1]);
+
+$names[0] = "Mudher";
+var_dump($names);
+
+unset($names[1]);
+var_dump($names);
+
+$names[] = "Wahyudi";
+var_dump($names);
+
+var_dump(count($names));
 
 
+
+$mudher = array(
+    "id" => "Mudher",
+    "name" => "Mudher Domili",
+    "age" => 27
+);
+
+var_dump($mudher);
+
+var_dump($mudher["age"]);
+
+$budi = [
+    "id" => "budi",
+    "name" => "Budi Test",
+    "age" => 35
+];
+var_dump($budi);
 ?>
