e5835dc07e6c97e8be91ff21a7adb1f61aecdb34
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-25 23:58:07 +0800

    Else If Statement

--- IfStatement.php
+++ IfStatement.php
@@ -1,10 +1,16 @@
 <?php
 
 $nilai = 70;
-$absen = 90;
+$absen = 60;
 
-if($nilai >= 75 && $absen>= 75){
-echo "Selamat anda Lulus" . PHP_EOL;
+if ($nilai >= 80 && $absen >= 80) {
+    echo "Nilai Anda A" . PHP_EOL;
+} else if ($nilai >= 70 && $absen >= 70) {
+    echo "Nilai Anda B" . PHP_EOL;
+} else if ($nilai >= 60 && $absen >= 60) {
+    echo "Nilai Anda C" . PHP_EOL;
+} else if ($nilai >= 50 && $absen >= 50) {
+    echo "Nilai Anda D" . PHP_EOL;
 } else {
-    echo "Maaf anda Tidak Lulus" . PHP_EOL;
+    echo "Nilai Anda E" . PHP_EOL;
 }
