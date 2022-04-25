4a98e0514e19e8c7848a3e96ebe3ef315d352ef7
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-26 00:10:16 +0800

    Perbandingan If Statement dan Switch Statement

---
+++ SwitchStatement.php
@@ -0,0 +1,30 @@
+<?php
+
+$nilai = "X";
+
+if($nilai == "A"){
+    echo "Anda lulus dengan sangat baik" . PHP_EOL;
+} elseif ($nilai == "B" || $nilai == "C"){
+    echo "Anda Lulus" . PHP_EOL;
+} elseif ($nilai == "D"){
+    echo "Anda tidak Lulus";
+} else {
+    echo "Mungkin Anda salah jurusan";
+};
+
+echo PHP_EOL;
+
+switch($nilai){
+    case "A":
+        echo "Anda lulus dengan sangat baik" . PHP_EOL;
+        break;
+        case "B":
+        case "C":
+            echo "Anda lulus" . PHP_EOL;
+            break;
+            case "D":
+                echo "Anda tidak Lulus" . PHP_EOL;
+                break;
+                default:
+                echo "Mungkin anda salah jurusan" . PHP_EOL;
+}
