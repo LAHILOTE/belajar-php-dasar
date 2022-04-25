4dd0bdda6826f042db6aa9e7ec288a90578412da
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-25 19:19:20 +0800

    Operator Array

--- OperatorArray.php
+++ OperatorArray.php
@@ -15,19 +15,28 @@ $a = [
     "first_name" => "Mudher",
     "last_name" => "Domili"
 ];
+$aa = [
+    "first_name" => "Mudher",
+    "last_name" => "Domili"
+];
 
 $b = [
     "last_name" => "Domili",
     "first_name" => "Mudher",
 ];
+$bb = [
+    "last_name" => "Domili",
+    "first_name" => "Mudhor",
+];
 // Operator Array Equality
 var_dump($a == $b);
 // Operator Array Identity
 var_dump($a === $b);
-echo "\n";
-
 // Operator Array Inequality
+var_dump($a != $bb);
 // Operator Array Inequality
+var_dump($a <> $bb);
 // Operator Array Nonidentity
-
+var_dump($a !== $aa);
+echo "\n";
 ?>
