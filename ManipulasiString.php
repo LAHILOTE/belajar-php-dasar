97cd7c348eea8beeadfd5ae03bd8e8a3581a3ab1
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-25 23:39:20 +0800

    Konversi String ke Number atau Kebalikannya

--- ManipulasiString.php
+++ ManipulasiString.php
@@ -3,4 +3,14 @@ $name = "Mudher Domili";
 
 echo "Name : " . $name . PHP_EOL;
 echo "Umur : " . 27 . PHP_EOL;
+
+
+$valueString = (string)100;
+var_dump($valueString);
+
+$valueInteger = (int)"100";
+var_dump($valueInteger);
+
+$valueFloat = (float)"100.123";
+var_dump($valueFloat);
 ?>
