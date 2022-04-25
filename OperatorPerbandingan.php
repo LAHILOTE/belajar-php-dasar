e1a97b2bed132b97002e04149aa010bbb77ee411
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-25 16:50:03 +0800

    Operator Perbandingan

--- OperatorPerbandingan.php
+++ OperatorPerbandingan.php
@@ -8,6 +8,7 @@ echo <<<TEXT
 TEXT;
 var_dump("10" === 10);
 
+echo "\n";
 
 echo <<<TEXT
 "10" tidak sama dengan (!=) 9 : 
@@ -17,6 +18,28 @@ echo <<<TEXT
 "10" tidak sama dengan (<>) 10 : 
 TEXT;
 var_dump("10" <> 10);
+echo <<<TEXT
+"10" tidak identik dengan (!==) 10 : 
+TEXT;
+var_dump("10" !== 10);
 
+echo "\n";
+
+echo <<<TEXT
+"10" kurang dari (<) 15 : 
+TEXT;
+var_dump("10" < 15);
+echo <<<TEXT
+"10" kurang dari atau sama dengan (<=) 10 : 
+TEXT;
+var_dump("10" <= 15);
+echo <<<TEXT
+"10" lebih dari (>) 15 : 
+TEXT;
+var_dump("10" > 15);
+echo <<<TEXT
+"10" lebih dari atau sama dengan (>=) 10 : 
+TEXT;
+var_dump("10" >= 10);
 
 ?>
