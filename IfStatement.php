0938c271eb2a1c5b0f2f980debc5d7f661a2b344
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-25 23:54:02 +0800

    Else Statement

--- IfStatement.php
+++ IfStatement.php
@@ -1,8 +1,10 @@
 <?php
 
-$nilai = 80;
+$nilai = 70;
 $absen = 90;
 
 if($nilai >= 75 && $absen>= 75){
 echo "Selamat anda Lulus" . PHP_EOL;
+} else {
+    echo "Maaf anda Tidak Lulus" . PHP_EOL;
 }
