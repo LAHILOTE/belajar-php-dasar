6149352b66402135047b838f7a74ab5c5790f093
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-25 18:20:30 +0800

    Increment dan Decrement

---
+++ IncrementDanDecrement.php
@@ -0,0 +1,16 @@
+<?php
+$a = 10;
+$b = 10;
+$c = 10;
+$d = 10;
+
+
+$a++; //Post Increment
+var_dump($a);
+++$b; //Pre Increment
+var_dump($b);
+$c--; //Post Decrement
+var_dump($c);
+--$d; //Pre Decrement
+var_dump($d);
+?>
