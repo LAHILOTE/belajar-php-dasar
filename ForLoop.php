d192f3ace902438d4e43dfeef4b9f5d9b82548ac
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-29 18:21:59 +0800

    Perulangan dengan init statement

--- ForLoop.php
+++ ForLoop.php
@@ -4,11 +4,25 @@
 //     echo "Ini adalah for loop" . PHP_EOL;
 // }
 
-// Kondisi Perulangan Dengan Kondisi
+// Perulangan Dengan Kondisi
+echo "Perulangan Dengan Kondisi";
+echo "-------------------------";
 $counter = 1;
 
 for ( ; $counter <= 10; ){
     echo "Ini adalah Loop ke-$counter" . PHP_EOL;
     $counter++;
 }
+
+echo PHP_EOL;
+
+// Kondisi Perulangan Dengan Init Statement
+echo "Perulangan Dengan Init Statement";
+echo "-------------------------";
+for($counter = 1; $counter <= 10; ){
+    echo "Hello For Loop : " . $counter . PHP_EOL;
+    $counter++;
+}
+
+
 ?>
