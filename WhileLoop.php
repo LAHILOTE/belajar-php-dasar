e475c973888ba6e34ab5932317f16d9d19fb0157
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-29 18:29:49 +0800

    Syntax Alternatif

--- WhileLoop.php
+++ WhileLoop.php
@@ -6,4 +6,11 @@ while($counter <= 10){
     $counter++;
 }
 
+// Syntax Alternatif
+echo "Syntax Alternatif" . PHP_EOL;
+while($counter <= 10) :
+    echo "Ini adalah While Loop ke-$counter" . PHP_EOL;
+    $counter++;
+endwhile;
+
 ?>
