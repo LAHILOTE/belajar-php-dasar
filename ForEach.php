0302d4168b2e298e3f096cf2eac433e111a0187d
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-29 19:00:55 +0800

    Dengan Foreach dan Key

--- ForEach.php
+++ ForEach.php
@@ -1,4 +1,6 @@
 <?php
+
+// Tanpa ForEach
 $names = ["Mudher", "Wahyu", "Domili"];
 
 for ($i = 0; $i < count($names); $i++){
@@ -7,7 +9,21 @@ for ($i = 0; $i < count($names); $i++){
 
 echo PHP_EOL;
 
-foreach ($names as $name){
-    echo "Data $name" . PHP_EOL;
+// Dengan ForEach
+foreach ($names as $index => $name){
+    echo "Data ke " . $index+1 . " " . $name . PHP_EOL;
+}
+
+echo PHP_EOL;
+
+// Dengan ForEach dan Key
+$person = [
+    "first_name" => "Mudher",
+    "middle_name" => "Wahyu",
+    "last_name" => "Domili",
+];
+
+foreach($person as $key => $value) {
+    echo "$key : $value" . PHP_EOL;
 }
 ?>
