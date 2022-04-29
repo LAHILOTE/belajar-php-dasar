d47a213725d79d0cd979afc80a7e995861b4fbee
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-29 18:18:32 +0800

    Perulangan dengan kondisi

--- ForLoop.php
+++ ForLoop.php
@@ -1,6 +1,14 @@
 <?php
 // Kondisi Perulangan Tanpa Henti
-for( ; ; ){
-    echo "Ini adalah for loop" . PHP_EOL;
+// for( ; ; ){
+//     echo "Ini adalah for loop" . PHP_EOL;
+// }
+
+// Kondisi Perulangan Dengan Kondisi
+$counter = 1;
+
+for ( ; $counter <= 10; ){
+    echo "Ini adalah Loop ke-$counter" . PHP_EOL;
+    $counter++;
 }
 ?>
