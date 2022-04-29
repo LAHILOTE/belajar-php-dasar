7abd2b7f760bc22e65c1dec13ca4059135b2e924
ASUS-GAMING\Mudher Domili <mudher.d@gmail.com>
2022-04-29 18:55:30 +0800

    dengan For Each

--- ForEach.php
+++ ForEach.php
@@ -5,4 +5,9 @@ for ($i = 0; $i < count($names); $i++){
     echo "Data ke-". $i+1 . " = $names[$i]" . PHP_EOL;
 }
 
+echo PHP_EOL;
+
+foreach ($names as $name){
+    echo "Data $name" . PHP_EOL;
+}
 ?>
